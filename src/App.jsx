36bd86153ccced4f333a34f258e7a60628fa160b
Muhammad Subhan <112649712+subhan-tahir@users.noreply.github.com>
2024-12-27 23:55:39 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,5 @@
 import React, { useState ,useEffect} from 'react'
-import Header from './components/Navbar/Header'
-import Hero from './components/Herosection/hero'
-import AboutGreener from './components/AboutGreener'
 import './App.css'
-import CustomizedCards from './components/Custimzedcards/CustomizedCards'
-import HouseWorker from './components/HouseWorker'
-import Counter from './components/counterGreener/Counter'
-import Packages from './components/packages/Packages'
-import GeoMessage from './components/GeoMessage'
-import AutoSlider from './components/AutoSlider'
-import Footer from './components/footer/Footer'
 import Routing from './Routing'
 import { FaArrowUp } from "react-icons/fa";
 
